aad069d34feb3f4c1499a4f1d76e08bcc3bc7789
DDShadowRU <ddcompanychanel@gmail.com>
2018-01-10 12:43:09 -0300

    Исправлен конфликт руд с IC

--- dev/blocks/ores.js
+++ dev/blocks/ores.js
@@ -1,7 +1,14 @@
+const BLOCK_TYPE_STONE = Block.createSpecialType({
+    base: 1,
+    solid: true,
+    destroytime: 3,
+    explosionres: 3
+}, "stone");
+
 IDRegistry.genBlockID("oreCopper");
 Block.createBlock("oreCopper", [
     {name: "Copper Ore", texture: [["oreCopper", 0]], inCreative: true}
-]);
+], BLOCK_TYPE_STONE);
 ToolAPI.registerBlockMaterial(BlockID.oreCopper, "stone");
 Block.setDestroyLevel("oreCopper", 2);
 
@@ -17,7 +24,7 @@ Block.registerDropFunction("oreCopper", function (coords, id, data, diggingLevel
 IDRegistry.genBlockID("oreTin");
 Block.createBlock("oreTin", [
     {name: "Tin Ore", texture: [["oreTin", 0]], inCreative: true}
-]);
+], BLOCK_TYPE_STONE);
 ToolAPI.registerBlockMaterial(BlockID.oreTin, "stone");
 Block.setDestroyLevel("oreTin", 2);
 
@@ -32,7 +39,7 @@ Block.registerDropFunction("oreTin", function (coords, id, data, diggingLevel) {
 IDRegistry.genBlockID("oreApatite");
 Block.createBlock("oreApatite", [
     {name: "Apatite Ore", texture: [["oreApatite", 0]], inCreative: true}
-]);
+], BLOCK_TYPE_STONE);
 ToolAPI.registerBlockMaterial(BlockID.oreApatite, "stone");
 Block.setDestroyLevel("oreApatite", 2);
 
