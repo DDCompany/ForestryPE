86b9f8140dc4c477613350fe6553efa6f7bf0e77
DDShadowRU <ddcompanychanel@gmail.com>
2018-04-15 19:24:08 +0300

    Изменена структура файлов проекта

---
+++ dev/core/items/other.js
@@ -0,0 +1,49 @@
+IDRegistry.genItemID("fertilizerBio");
+Item.createItem("fertilizerBio", "Compost", {name: "fertilizerBio", meta: 0}, {});
+
+IDRegistry.genItemID("mouldyWheat");
+Item.createItem("mouldyWheat", "Mouldy wheat", {name: "mouldyWheat", meta: 0}, {});
+
+IDRegistry.genItemID("decayingWheat");
+Item.createItem("decayingWheat", "Decaying wheat", {name: "decayingWheat", meta: 0}, {});
+
+IDRegistry.genItemID("mulch");
+Item.createItem("mulch", "Mulch", {name: "mulch", meta: 0}, {});
+
+IDRegistry.genItemID("woodPulp");
+Item.createItem("woodPulp", "Wood pulp", {name: "woodPulp", meta: 0}, {});
+
+IDRegistry.genItemID("carton");
+Item.createItem("carton", "Carton", {name: "carton", meta: 0}, {});
+
+IDRegistry.genItemID("fertilizerCompound");
+Item.createItem("fertilizerCompound", "Fertilizer", {name: "fertilizerCompound", meta: 0}, {});
+
+IDRegistry.genItemID("ash");
+Item.createItem("ash", "Ash", {name: "ash", meta: 0}, {});
+
+Callback.addCallback("PostLoaded", function () {
+    Recipes.addShaped({id: ItemID.fertilizerBio, count: 4, data: 0}, [
+        " w ",
+        "wdw",
+        " w "
+    ], ['w', 296, -1, 'd', 3, -1]);
+
+    Recipes.addShaped({id: ItemID.fertilizerBio, count: 1, data: 0}, [
+        " w ",
+        "wdw",
+        " w "
+    ], ['w', ItemID.ash, -1, 'd', 3, -1]);
+
+    Recipes.addShaped({id: ItemID.fertilizerCompound, count: 8, data: 0}, [
+        " s ",
+        " w ",
+        " s "
+    ], ['w', ItemID.apatite, -1, 's', 12, -1]);
+
+    Recipes.addShaped({id: ItemID.fertilizerCompound, count: 16, data: 0}, [
+        "sss",
+        "sws",
+        "sss"
+    ], ['w', ItemID.apatite, -1, 's', ItemID.ash, -1]);
+});
---
+++ dev/factory/items/chipsets.js
@@ -0,0 +1,12 @@
+IDRegistry.genItemID("chipsetBasic");
+Item.createItem("chipsetBasic", "Basic Circuit Board", {name: "chipsetBasic", meta: 0}, {});
+
+IDRegistry.genItemID("chipsetEnhanced");
+Item.createItem("chipsetEnhanced", "Enhanced Circuit Board", {name: "chipsetEnhanced", meta: 0}, {});
+
+IDRegistry.genItemID("chipsetIntricate");
+Item.createItem("chipsetIntricate", "Intricate Circuit Board", {name: "chipsetIntricate", meta: 0}, {});
+
+IDRegistry.genItemID("chipsetRefined");
+Item.createItem("chipsetRefined", "Refined Circuit Board", {name: "chipsetRefined", meta: 0}, {});
+
