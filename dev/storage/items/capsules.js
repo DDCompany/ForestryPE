fa4ee5baabeb501d68010ff634d9f58611408976
DDShadowRU <ddcompanychanel@gmail.com>
2018-04-18 20:03:21 +0300

    Добавлен крафт тортов с помощью капсул с молоком

--- dev/storage/items/capsules.js
+++ dev/storage/items/capsules.js
@@ -20,20 +20,32 @@ function registerLiquidContainer(suffix, liquid, food, isNative, isHot) {
     if (food) {
         if (!isHot) {
             IDRegistry.genItemID("waxCapsule" + suffix);
-            Item.createFoodItem("waxCapsule" + suffix, suffix + " wax Capsule", {name: "waxCapsule" + suffix, meta: 0}, {food: food});
-            LiquidRegistry.registerItem(liquid, {id: ItemID.waxCapsuleEmpty, data: 0}, {id: ItemID["waxCapsule" + suffix], data: 0});
+            Item.createFoodItem("waxCapsule" + suffix, suffix + " wax Capsule", {
+                name: "waxCapsule" + suffix,
+                meta: 0
+            }, {food: food});
+            LiquidRegistry.registerItem(liquid, {
+                id: ItemID.waxCapsuleEmpty,
+                data: 0
+            }, {id: ItemID["waxCapsule" + suffix], data: 0});
         }
 
         IDRegistry.genItemID("can" + suffix);
         Item.createFoodItem("can" + suffix, suffix + " Can", {name: "can" + suffix, meta: 0});
 
         IDRegistry.genItemID("refractory" + suffix);
-        Item.createFoodItem("refractory" + suffix, suffix + " Capsule", {name: "refractory" + suffix, meta: 0}, {food: food});
+        Item.createFoodItem("refractory" + suffix, suffix + " Capsule", {
+            name: "refractory" + suffix,
+            meta: 0
+        }, {food: food});
     } else {
         if (!isHot) {
             IDRegistry.genItemID("waxCapsule" + suffix);
             Item.createItem("waxCapsule" + suffix, suffix + " wax Capsule", {name: "waxCapsule" + suffix, meta: 0});
-            LiquidRegistry.registerItem(liquid, {id: ItemID.waxCapsuleEmpty, data: 0}, {id: ItemID["waxCapsule" + suffix], data: 0});
+            LiquidRegistry.registerItem(liquid, {
+                id: ItemID.waxCapsuleEmpty,
+                data: 0
+            }, {id: ItemID["waxCapsule" + suffix], data: 0});
         }
 
         IDRegistry.genItemID("can" + suffix);
@@ -44,7 +56,10 @@ function registerLiquidContainer(suffix, liquid, food, isNative, isHot) {
     }
 
     LiquidRegistry.registerItem(liquid, {id: ItemID.canEmpty, data: 0}, {id: ItemID["can" + suffix], data: 0});
-    LiquidRegistry.registerItem(liquid, {id: ItemID.refractoryEmpty, data: 0}, {id: ItemID["refractory" + suffix], data: 0});
+    LiquidRegistry.registerItem(liquid, {id: ItemID.refractoryEmpty, data: 0}, {
+        id: ItemID["refractory" + suffix],
+        data: 0
+    });
 }
 
 registerLiquidContainer("Water", "water", 0, true);
@@ -98,4 +113,16 @@ Callback.addCallback("PostLoaded", function () {
         "   ",
         "   "
     ], ['x', ItemID.refractoryWax, 0]);
+
+    let milkCapsules = [ItemID.canMilk, ItemID.refractoryMilk, ItemID.waxCapsuleMilk];
+
+    for (let key in milkCapsules) {
+        let capsuleId = milkCapsules[key];
+
+        Recipes.addShaped({id: 354, count: 1, data: 0}, [
+            "AAA",
+            "BEB",
+            "CCC"
+        ], ['A', capsuleId, 0, 'B', 353, 0, 'C', 296, 0, 'E', 344, 0]);
+    }
 });
