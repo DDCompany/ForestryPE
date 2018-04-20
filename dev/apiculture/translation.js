c629533252858cbb0f41cf26842afc650bdff245
DDShadowRU <ddcompanychanel@gmail.com>
2018-04-20 22:25:56 +0300

    Единый файл локализации разделён на более мелкие

---
+++ dev/apiculture/translation.js
@@ -0,0 +1,172 @@
+//Machines
+Translation.addTranslation("Apiary", {ru: "Пасека"});
+Translation.addTranslation("Bee House", {ru: "Пчелиный домик"});
+Translation.addTranslation("Apiarist's Chest", {ru: "Сундук пчеловода"});
+Translation.addTranslation("Alveary", {ru: "Пчелиный улей"});
+Translation.addTranslation("Alveary Fan", {ru: "Вентилятор улья"});
+Translation.addTranslation("Alveary Heater", {ru: "Обогреватель улья"});
+Translation.addTranslation("Alveary Hygroregulator", {ru: "Кондиционер улья"});
+Translation.addTranslation("Alveary Stabiliser", {ru: "Стабилизатор улья"});
+Translation.addTranslation("Alveary Controller", {ru: "Котроллер огромного улья"});
+
+//Beehives
+Translation.addTranslation("Forest Hive", {ru: "Лесной улей"});
+Translation.addTranslation("Meadows Hive", {ru: "Луговой улей"});
+Translation.addTranslation("Modest Hive", {ru: "Пустынный улей"});
+Translation.addTranslation("Tropical Hive", {ru: "Тропический улей"});
+Translation.addTranslation("Wintry Hive", {ru: "Снежный улей"});
+Translation.addTranslation("Marshy Hive", {ru: "Болотный улей"});
+Translation.addTranslation("Ender Hive", {ru: "Улей Края"});
+
+//Tools
+Translation.addTranslation("Scoop", {ru: "Сачок"});
+
+//Storage
+Translation.addTranslation("Wax Block", {ru: "Блок воска"});
+
+//Combs
+Translation.addTranslation("Cocoa Comb", {ru: "Какао-соты"});
+Translation.addTranslation("Dripping Comb", {ru: "Капающие соты"});
+Translation.addTranslation("Honey Comb", {ru: "Медовые соты"});
+Translation.addTranslation("Frozen Comb", {ru: "Морозные соты"});
+Translation.addTranslation("Mellow Comb", {ru: "Выдержанные соты"});
+Translation.addTranslation("Mossy Comb", {ru: "Замшелые соты"});
+Translation.addTranslation("Mysterious Comb", {ru: "Таинственные соты"});
+Translation.addTranslation("Parched Comb", {ru: "Пересохшие соты"});
+Translation.addTranslation("Powdery Comb", {ru: "Рыхлые соты"});
+Translation.addTranslation("Silky Comb", {ru: "Шелковые соты"});
+Translation.addTranslation("Simmering Comb", {ru: "Кипящие соты"});
+Translation.addTranslation("Stringy Comb", {ru: "Вязкие соты"});
+Translation.addTranslation("Wheaten Comb", {ru: "Пшеничные соты"});
+Translation.addTranslation("Irradiated Comb", {ru: "Облученные соты"});
+
+Translation.addTranslation("Cocoa Comb Block", {ru: "Блок какао-сот"});
+Translation.addTranslation("Dripping Comb Block", {ru: "Блок капающих сот"});
+Translation.addTranslation("Honey Comb Block", {ru: "Блок медовых сот"});
+Translation.addTranslation("Frozen Comb Block", {ru: "Блок морозных сот"});
+Translation.addTranslation("Mellow Comb Block", {ru: "Блок выдержанных сот"});
+Translation.addTranslation("Mossy Comb Block", {ru: "Блок замшелых сот"});
+Translation.addTranslation("Mysterious Comb Block", {ru: "Блок таинственных сот"});
+Translation.addTranslation("Parched Comb Block", {ru: "Блок пересохших сот"});
+Translation.addTranslation("Powdery Comb Block", {ru: "Блок рыхлых сот"});
+Translation.addTranslation("Silky Comb Block", {ru: "Блок шелковых сот"});
+Translation.addTranslation("Simmering Comb Block", {ru: "Блок кипящих сот"});
+Translation.addTranslation("Stringy Comb Block", {ru: "Блок вязких сот"});
+Translation.addTranslation("Wheaten Comb Block", {ru: "Блок пшеничных сот"});
+Translation.addTranslation("Irradiated Comb Block", {ru: "Блок облученных сот"});
+
+//Frames
+Translation.addTranslation("Impregnated Frame", {ru: "Пропитанная рамка"});
+Translation.addTranslation("Untreated Frame", {ru: "Необработанная рамка"});
+Translation.addTranslation("Proven Frame", {ru: "Проверенная рамка"});
+
+//Armor
+Translation.addTranslation("Apiarist's Hat", {ru: "Защитная маска пчеловода"});
+Translation.addTranslation("Apiarist's Shirt", {ru: "Куртка пчеловода"});
+Translation.addTranslation("Apiarist's Pants", {ru: "Штаны пчеловода"});
+Translation.addTranslation("Apiarist's Shoes", {ru: "Ботинки пчеловода"});
+
+//Other
+Translation.addTranslation("Honey Drop", {ru: "Капля мёда"});
+Translation.addTranslation("Beeswax", {ru: "Пчелиный воск"});
+Translation.addTranslation("Honeydew", {ru: "Медвяная роса"});
+Translation.addTranslation("Propolis", {ru: "Прополис"});
+Translation.addTranslation("Refractory Wax", {ru: "Огнеупорный воск"});
+Translation.addTranslation("Phosphor", {ru: "Фосфор"});
+Translation.addTranslation("Crystalline Pollen Cluster", {ru: "Скопление кристаллической пыльцы"});
+Translation.addTranslation("Silky Propolis", {ru: "Липкий прополис"});
+Translation.addTranslation("Pulsating Propolis", {ru: "Пульсирующий прополис"});
+Translation.addTranslation("Royal Jelly", {ru: "Маточное молочко"});
+Translation.addTranslation("Pollen Cluster", {ru: "Скопление пыльцы"});
+Translation.addTranslation("Ice Shard", {ru: "Осколок льда"});
+Translation.addTranslation("Silk Wisp", {ru: "Клочок шёлка"});
+Translation.addTranslation("Wax Cast", {ru: "Восковая форма"});
+Translation.addTranslation("Pulsating Mesh", {ru: "Пульсирующая сеть"});
+Translation.addTranslation("Honeyed Slice", {ru: "Бутерброд с мёдом"});
+Translation.addTranslation("Impregnated Stick", {ru: "Полированная палка"});
+Translation.addTranslation("Woven Silk", {ru: "Тканый шёлк"});
+Translation.addTranslation("Portable Analyzer", {ru: "Портативный анализатор"});
+Translation.addTranslation("Honey Pot", {ru: "Горшочек мёда"});
+Translation.addTranslation("Scented Paneling", {ru: "Пропитанная обшивка"});
+Translation.addTranslation("Ambrosia", {ru: "Амброзия"});
+
+Translation.addTranslation("apiary.error.flowers", {en: "No flowers found", ru: "Цветы не найдены"});
+Translation.addTranslation("apiary.error.climate", {en: "Incorrect temperature", ru: "Некорректная температура"});
+Translation.addTranslation("apiary.error.humidity", {en: "Incorrect humidity", ru: "Некорректная влажность"});
+Translation.addTranslation("apiary.error.sky", {en: "Can't see sky", ru: "Не видно неба"});
+Translation.addTranslation("apiary.error.night", {
+    en: "These bees do not work at night",
+    ru: "Эти пчёлы не работают ночью"
+});
+Translation.addTranslation("apiary.error.rain", {
+    en: "These bees do not work during the rain",
+    ru: "Эти пчёлы не работают во время дождя"
+});
+
+Translation.addTranslation("bees.lifespan.shorted", {en: "Shorted", ru: "Кратчайший"});
+Translation.addTranslation("bees.lifespan.shortest", {en: "Shortest", ru: "Наикратчайший"});
+Translation.addTranslation("bees.lifespan.short", {en: "Short", ru: "Краткий"});
+Translation.addTranslation("bees.lifespan.shortened", {en: "Shortened", ru: "Короткий"});
+Translation.addTranslation("bees.lifespan.normal", {en: "Normal", ru: "Обычный"});
+Translation.addTranslation("bees.lifespan.elongated", {en: "Elongated", ru: "Продлённый"});
+Translation.addTranslation("bees.lifespan.long", {en: "Long", ru: "Длинный"});
+Translation.addTranslation("bees.lifespan.longer", {en: "Longer", ru: "Очень длинный"});
+Translation.addTranslation("bees.lifespan.longest", {en: "Longest", ru: "Длиннейший"});
+
+Translation.addTranslation("bees.speed.slowest", {en: "Slowest", ru: "Очень медленная"});
+Translation.addTranslation("bees.speed.slower", {en: "Slower", ru: "Медленная"});
+Translation.addTranslation("bees.speed.slow", {en: "Slow", ru: "Неторопливая"});
+Translation.addTranslation("bees.speed.normal", {en: "Normal", ru: "Обычная"});
+Translation.addTranslation("bees.speed.fast", {en: "Fast", ru: "Быстрая"});
+Translation.addTranslation("bees.speed.faster", {en: "Faster", ru: "Очень быстрая"});
+Translation.addTranslation("bees.speed.fastest", {en: "Fastest", ru: "Наибыстрейшая"});
+
+Translation.addTranslation("bees.effect.none", {en: "None", ru: "Нет"});
+Translation.addTranslation("bees.effect.aggress", {en: "Aggress", ru: "Агрессивность"});
+Translation.addTranslation("bees.effect.beatific", {en: "Beatific", ru: "Блаженность"});
+Translation.addTranslation("bees.effect.creeper", {en: "Creeper", ru: "Крипер"});
+Translation.addTranslation("bees.effect.drunkard", {en: "Drunkard", ru: "Опьянение"});
+Translation.addTranslation("bees.effect.explorer", {en: "Explorer", ru: "Исследовательность"});
+Translation.addTranslation("bees.effect.flammable", {en: "Flammable", ru: "Воспламенение"});
+Translation.addTranslation("bees.effect.freezing", {en: "Freezing", ru: "Замораживание"});
+Translation.addTranslation("bees.effect.heroic", {en: "Heroic", ru: "Геройство"});
+Translation.addTranslation("bees.effect.poison", {en: "Poison", ru: "Отвращение"});
+Translation.addTranslation("bees.effect.radiact", {en: "Radioact", ru: "Радиоактивность"});
+Translation.addTranslation("bees.effect.reanimation", {en: "Reanimation", ru: "Воскрешение"});
+Translation.addTranslation("bees.effect.repulsion", {en: "Repulsion", ru: "Отвращение"});
+Translation.addTranslation("bees.effect.ends", {en: "Ends", ru: "Окончание"});
+
+Translation.addTranslation("climate.icy", {en: "Icy", ru: "Ледяная"});
+Translation.addTranslation("climate.cold", {en: "Cold", ru: "Холодная"});
+Translation.addTranslation("climate.normal", {en: "Normal", ru: "Обычная"});
+Translation.addTranslation("climate.warm", {en: "Warm", ru: "Тёплая"});
+Translation.addTranslation("climate.hot", {en: "Hot", ru: "Горячая"});
+Translation.addTranslation("climate.hellish", {en: "Hellish", ru: "Адская"});
+
+Translation.addTranslation("humidity.arid", {en: "Arid", ru: "Сухая"});
+Translation.addTranslation("humidity.damp", {en: "Damp", ru: "Сырая"});
+Translation.addTranslation("humidity.normal", {en: "Normal", ru: "Обычная"});
+
+Translation.addTranslation("analyzer.active", {en: "Active", ru: "Активный"});
+Translation.addTranslation("analyzer.speed", {en: "Speed", ru: "Скорость"});
+Translation.addTranslation("analyzer.inactive", {en: "Inactive", ru: "Неактивный"});
+Translation.addTranslation("analyzer.climate", {en: "Climate", ru: "Климат"});
+Translation.addTranslation("analyzer.tolerance", {en: "Tolerance", ru: "Устойчивость"});
+Translation.addTranslation("analyzer.humidity", {en: "Humidity", ru: "Влажность"});
+Translation.addTranslation("analyzer.diurnal", {en: "Diurnal", ru: "Дневная"});
+Translation.addTranslation("analyzer.nocturnal", {en: "Nocturnal", ru: "Ночная"});
+Translation.addTranslation("analyzer.flyer", {en: "Flyer", ru: "Летун"});
+Translation.addTranslation("analyzer.cave", {en: "Cave", ru: "Пещерная"});
+Translation.addTranslation("analyzer.pristine", {en: "Pristine Stock", ru: "Чистая порода"});
+Translation.addTranslation("analyzer.ignoble", {en: "Ignoble Stock", ru: "Низкая порода"});
+Translation.addTranslation("analyzer.generation", {en: "Generations in Captivity", ru: "Поколение в неволе"});
+Translation.addTranslation("analyzer.produce", {en: "Possible produce", ru: "Возможная продукция"});
+Translation.addTranslation("analyzer.specialty", {en: "Possible specialty", ru: "Возможная специальная продукция"});
+Translation.addTranslation("analyzer.species", {en: "Species", ru: "Вид"});
+Translation.addTranslation("analyzer.lifespan", {en: "Lifespan", ru: "Срок жизни"});
+Translation.addTranslation("analyzer.flowers", {en: "Flowers type", ru: "Цветы"});
+Translation.addTranslation("analyzer.fertility", {en: "Fertility", ru: "Плодовитость"});
+Translation.addTranslation("analyzer.territory", {en: "Territory", ru: "Территория"});
+Translation.addTranslation("analyzer.effect", {en: "Effect", ru: "Эффект"});
+Translation.addTranslation("analyzer.yes", {en: "Yes", ru: "Да"});
+Translation.addTranslation("analyzer.no", {en: "No", ru: "Нет"});
---
+++ dev/core/translation.js
@@ -0,0 +1,54 @@
+//Glass
+Translation.addTranslation("Black Glass", {ru: "Черное стекло"});
+Translation.addTranslation("Red Glass", {ru: "Красное стекло"});
+Translation.addTranslation("Green Glass", {ru: "Зелёное стекло"});
+Translation.addTranslation("Brown Glass", {ru: "Коричневое стекло"});
+Translation.addTranslation("Blue Glass", {ru: "Голубое стекло"});
+Translation.addTranslation("Purple Glass", {ru: "Фиолетовое стекло"});
+Translation.addTranslation("Cyan Glass", {ru: "Берюзовое стекло"});
+Translation.addTranslation("Light Gray glass", {ru: "Светло-серое стекло"});
+Translation.addTranslation("Gray Glass", {ru: "Серое стекло"});
+Translation.addTranslation("Pink Glass", {ru: "Розовое стекло"});
+Translation.addTranslation("Lime Glass", {ru: "Светло-зеленое стекло"});
+Translation.addTranslation("Yellow Glass", {ru: "Желтое стекло"});
+Translation.addTranslation("Light Blue Glass", {ru: "Светло-синее стекло"});
+Translation.addTranslation("Magenta Glass", {ru: "Пурпурное стекло"});
+Translation.addTranslation("Orange Glass", {ru: "Оранжевое стекло"});
+Translation.addTranslation("White Glass", {ru: "Белое стекло"});
+
+//Ores
+Translation.addTranslation("Apatite Ore", {ru: "Апатитовая руда"});
+Translation.addTranslation("Copper Ore", {ru: "Медная руда"});
+Translation.addTranslation("Tin Ore", {ru: "Оловянная руда"});
+
+//Ingots
+Translation.addTranslation("Copper Ingot", {ru: "Медный слиток"});
+Translation.addTranslation("Tin Ingot", {ru: "Оловянный слиток"});
+Translation.addTranslation("Bronze Ingot", {ru: "Бронзовый слиток"});
+
+//Storage
+Translation.addTranslation("Block of Charcoal", {ru: "Блок угля"});
+Translation.addTranslation("Ash Block", {ru: "Кучка золы"});
+Translation.addTranslation("Ash Bricks", {ru: "Кирпичи из золы"});
+Translation.addTranslation("Block of Apatite", {ru: "Апатитовый блок"});
+
+//Other
+Translation.addTranslation("Ash", {ru: "Пепел"});
+Translation.addTranslation("Mulch", {ru: "Мульча"});
+Translation.addTranslation("Wood Pulp", {ru: "Древесная масса"});
+Translation.addTranslation("Carton", {ru: "Картон"});
+Translation.addTranslation("Pickaxe Kit", {ru: "Сборная кирка"});
+Translation.addTranslation("Shovel Kit", {ru: "Сборная лопата"});
+Translation.addTranslation("Fertilizer", {ru: "Минеральное удобрение"});
+Translation.addTranslation("Apatite", {ru: "Апатит"});
+Translation.addTranslation("Mouldy Wheat", {ru: "Плесневелая пшеница"});
+Translation.addTranslation("Decaying Wheat", {ru: "Загнивающая пшеница"});
+Translation.addTranslation("Bog Earth", {ru: "Болотная земля"});
+Translation.addTranslation("Humus", {ru: "Гумус"});
+Translation.addTranslation("Peat", {ru: "Торф"});
+Translation.addTranslation("Bituminous Peat", {ru: "Смолистый торф"});
+Translation.addTranslation("Compost", {ru: "Компост"});
+Translation.addTranslation("Survivalist's Pickaxe", {ru: "Кирка выживающего"});
+Translation.addTranslation("Survivalist's Shovel", {ru: "Лопата выживающего"});
+Translation.addTranslation("Broken Pickaxe", {ru: "Сломанная кирка"});
+Translation.addTranslation("Broken Shovel", {ru: "Сломанная лопата"});
