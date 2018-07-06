96590d856075cb615f86dac6cddcae14060f6d27
DDShadowRU <ddcompanychanel@gmail.com>
2018-07-06 17:48:56 +0300

    Изменены айди бронзовых инструментов, переименованы их текстуры

--- dev/core/items/tools.js
+++ dev/core/items/tools.js
@@ -17,25 +17,25 @@ Item.createItem("brokenBronzePickaxe", "Broken Pickaxe", {name: "brokenBronzePic
 IDRegistry.genItemID("brokenBronzeShovel");
 Item.createItem("brokenBronzeShovel", "Broken Shovel", {name: "brokenBronzeShovel", meta: 0}, {stack: 1});
 
-IDRegistry.genItemID("bronzePickaxe");
-Item.createItem("bronzePickaxe", "Survivalist's Pickaxe", {name: "bronzePickaxe", meta: 0}, {stack: 1});
-ToolAPI.setTool(ItemID.bronzePickaxe, "bronze", ToolType.pickaxe, ItemID.brokenBronzePickaxe);
+IDRegistry.genItemID("forestryBronzePickaxe");
+Item.createItem("forestryBronzePickaxe", "Survivalist's Pickaxe", {name: "forestryBronzePickaxe", meta: 0}, {stack: 1});
+ToolAPI.setTool(ItemID.forestryBronzePickaxe, "bronze", ToolType.pickaxe, ItemID.brokenBronzePickaxe);
 
-IDRegistry.genItemID("bronzeShovel");
-Item.createItem("bronzeShovel", "Survivalist's Shovel", {name: "bronzeShovel", meta: 0}, {stack: 1});
-ToolAPI.setTool(ItemID.bronzeShovel, "bronze", ToolType.shovel, ItemID.brokenBronzeShovel);
+IDRegistry.genItemID("forestryBronzeShovel");
+Item.createItem("forestryBronzeShovel", "Survivalist's Shovel", {name: "forestryBronzeShovel", meta: 0}, {stack: 1});
+ToolAPI.setTool(ItemID.forestryBronzeShovel, "bronze", ToolType.shovel, ItemID.brokenBronzeShovel);
 
 IDRegistry.genItemID("scoop");
 Item.createItem("scoop", "Scoop", {name: "scoop", meta: 0}, {stack: 1});
 ToolAPI.setTool(ItemID.scoop, "scoop", ToolType.scoop);
 
 Item.registerUseFunction("kitPickaxe", function (coords) {
-    World.drop(coords.relative.x + 0.5, coords.relative.y + 0.1, coords.relative.z + 0.5, ItemID.bronzePickaxe, 1, 0);
+    World.drop(coords.relative.x + 0.5, coords.relative.y + 0.1, coords.relative.z + 0.5, ItemID.forestryBronzePickaxe, 1, 0);
     Player.decreaseCarriedItem(1);
 });
 
 Item.registerUseFunction("kitShovel", function (coords) {
-    World.drop(coords.relative.x + 0.5, coords.relative.y + 0.1, coords.relative.z + 0.5, ItemID.bronzeShovel, 1, 0);
+    World.drop(coords.relative.x + 0.5, coords.relative.y + 0.1, coords.relative.z + 0.5, ItemID.forestryBronzeShovel, 1, 0);
     Player.decreaseCarriedItem(1);
 });
 
@@ -46,13 +46,13 @@ Callback.addCallback("PostLoaded", function () {
         " s "
     ], ['w', 35, -1, 's', 280, -1]);
 
-    Recipes.addShaped({id: ItemID.bronzePickaxe, count: 1, data: 0}, [
+    Recipes.addShaped({id: ItemID.forestryBronzePickaxe, count: 1, data: 0}, [
         " s ",
         " s ",
         "bbb"
     ], ['s', 280, 0, 'b', ItemID.ingotBronze, 0]);
 
-    Recipes.addShaped({id: ItemID.bronzeShovel, count: 1, data: 0}, [
+    Recipes.addShaped({id: ItemID.forestryBronzeShovel, count: 1, data: 0}, [
         "s",
         "s",
         "b"
@@ -60,9 +60,9 @@ Callback.addCallback("PostLoaded", function () {
 
     Recipes.addShaped({id: ItemID.kitPickaxe, count: 1, data: 0}, [
         "pc",
-    ], ['p', ItemID.bronzePickaxe, 0, 'c', ItemID.carton, -1]);
+    ], ['p', ItemID.forestryBronzePickaxe, 0, 'c', ItemID.carton, -1]);
 
     Recipes.addShaped({id: ItemID.kitShovel, count: 1, data: 0}, [
         "pc",
-    ], ['p', ItemID.bronzeShovel, 0, 'c', ItemID.carton, -1]);
+    ], ['p', ItemID.forestryBronzeShovel, 0, 'c', ItemID.carton, -1]);
 });
