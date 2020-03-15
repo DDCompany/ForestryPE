f5016eedd286be07146347028d902ce41eaf3e60
DDShadowRU <ddcompanychanel@gmail.com>
2020-03-15 10:47:36 -0400

    Добавлена команда для создания релиза

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,27 @@ module.exports = function (grunt) {
  * Built at ${(new Date()).toDateString()}
  * © DDCompany (https://vk.com/id331953744)
  */\n`;
+    const modFiles = [
+        'gui/**',
+        'lib/**',
+        'res/**',
+        "build.config",
+        "config.json",
+        "launcher.js",
+        "mod.info",
+        "mod_icon.png",
+        "main.js"
+    ];
+
+    function createModInfo(version = "") {
+        grunt.file.delete("mod.info");
+        grunt.file.write("mod.info", JSON.stringify({
+            name: "ForestryPE",
+            author: "DDCompany",
+            description: "Need Wood? Get Forestry!",
+            version: version
+        }, null, '\t'))
+    }
 
     function readIncludes(dir) {
         let arr = [];
@@ -38,11 +59,54 @@ module.exports = function (grunt) {
                 src: readIncludes("dev"),
                 dest: 'main.js',
             }
+        },
+
+        compress: {
+            icmod: {
+                options: {
+                    mode: "zip",
+                    archive: function () {
+                        return `releases/ForestryPE-${grunt.option("ver")}.icmod`
+                    }
+                },
+                files: [
+                    {
+                        src: modFiles,
+                        dest: "Forestry/"
+                    }
+                ]
+            },
+            zip: {
+                options: {
+                    mode: "zip",
+                    archive: function () {
+                        return `releases/ForestryPE-${grunt.option("ver")}.zip`
+                    }
+                },
+                files: [
+                    {
+                        src: modFiles
+                    }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', "", function () {
+        grunt.task.run("concat");
+        createModInfo()
+    });
 
+    grunt.registerTask('release', "", function () {
+        const version = grunt.option("ver");
+        createModInfo(version);
+        grunt.task.run("concat");
+
+        if (!grunt.option("zip")) {
+            grunt.task.run("compress:icmod");
+        } else grunt.task.run("compress:zip");
+    });
 };
