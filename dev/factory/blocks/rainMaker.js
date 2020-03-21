9dd4dca9d39655072d9378ef49f7a8dcbdd7a8f6
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-03-21 20:02:43 +0300

    Оптимизация рендера вызывателя дождя

--- dev/factory/blocks/rainMaker.js
+++ dev/factory/blocks/rainMaker.js
@@ -26,22 +26,19 @@ Block.registerDropFunctionForID(BlockID.rainmaker, function () {
     return [[ItemID.rainMaker, 1, 0]];
 });
 
-Block.setBlockShape(BlockID.rainmaker, {x: 0.3, y: 0, z: 0.3}, {x: 0.6, y: 0.6, z: 0.6});
+{
+    let render = new ICRender.Model();
+    let model = BlockRenderer.createModel();
+    BlockRenderer.setStaticICRender(BlockID.rainmaker, 0, render);
 
-BlockRenderer.addRenderCallback(BlockID.rainmaker, function (api, coords) {
+    model.addBox(0, 0, 0, 1, 0.07, 1, BlockID.rainmaker, 0);
+    model.addBox(0.4, 0.07, 0.4, 0.6, 0.97, 0.6, BlockID.rainmaker, 0);
+    model.addBox(0.1, 0.44, 0.44, 0.88, 0.57, 0.57, 35, 15);
+    model.addBox(0.2, 0.15, 0.25, 0.3, 0.85, 0.75, [["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 3]]);
+    model.addBox(0.7, 0.15, 0.25, 0.8, 0.85, 0.75, [["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 3]]);
 
-    api.renderBoxId(coords.x, coords.y, coords.z, 0, 0, 0, 1, 0.07, 1, BlockID.rainmaker, 0);
-
-    api.renderBoxId(coords.x, coords.y, coords.z, 0.4, 0.07, 0.4, 0.6, 0.97, 0.6, BlockID.rainmaker, 0);
-
-    api.renderBoxId(coords.x, coords.y, coords.z, 0.1, 0.44, 0.44, 0.88, 0.57, 0.57, 35, 15);
-
-    api.renderModel(coords.x, coords.y, coords.z, BlockRenderer.createTexturedBox(0.2, 0.15, 0.25, 0.3, 0.85, 0.75, [["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 3]]));
-    api.renderModel(coords.x, coords.y, coords.z, BlockRenderer.createTexturedBox(0.7, 0.15, 0.25, 0.8, 0.85, 0.75, [["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 4], ["rainmaker", 3]]));
-
-});
-
-BlockRenderer.enableCustomRender(BlockID.rainmaker);
+    render.addEntry(model);
+}
 
 IDRegistry.genItemID("iodineCapsule");
 Item.createItem("iodineCapsule", "Iodine Capsule", {name: "iodineCapsule", meta: 0}, {});
