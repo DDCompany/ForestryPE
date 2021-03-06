6db5c0277f3b72ea2ea47a643721ac4ae5397ecf
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2021-03-06 19:30:45 +0300

    Added toolchain

---
+++ src/dev/header.ts
@@ -0,0 +1,24 @@
+/*
+ *    ______                  _               _____  ______
+ *   |  ____|                | |             |  __ \\|  ____|
+ *   | |__ ___  _ __ ___  ___| |_ _ __ _   _ | |__) | |__
+ *   |  __/ _ \\|'__/ _ \\/ __| __| '__| | | | |  ___/|  __|
+ *   | | | (_) ||| __/\\__ \\ |_| |  | |_| | | |    | |____
+ *   |_| \\___/|_ \\___||___/\\__|_| \\__, | |_|    |______|
+ *                                    __/ |
+ *                                   |___/
+ *
+ * Terms of use:
+ *  - Forbidden to distribute the library on third-party sources
+ *    without links to the official group (https://vk.com/forestry_pe)
+ *  - Forbidden to change the code of this mod
+ *  - Forbidden to explicitly copy the code to other libraries or mods
+ *  - Using the mod you automatically agree to the conditions described above
+ *
+ * Warning!
+ * All ideas and textures belong to the original author - https://github.com/ForestryMC/ForestryMC
+ *
+ * Port by DDCompany (https://vk.com/forestry_pe)
+ */
+
+Logger.Log("Forestry successfully loaded", "INFO");
---
+++ src/launcher.js
@@ -0,0 +1,4 @@
+ConfigureMultiplayer({
+    isClientOnly: false,
+});
+Launch();
