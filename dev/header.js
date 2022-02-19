babb5d4dde9b9f9db94f8b0c2752017e8ee43271
DDShadowRU <ddcompanychanel@gmail.com>
2022-02-19 18:52:13 +0300

    Closed #24: override of global Entity.getArmorSlot

--- dev/header.js
+++ dev/header.js
@@ -10,14 +10,9 @@ const startTime = java.lang.System.currentTimeMillis();
 const APATITE_GEN_BIOMES = [3, 131, 34, 162];
 const EU = EnergyTypeRegistry.assureEnergyType("Eu", 1);
 const RF = EnergyTypeRegistry.assureEnergyType("RF", 0.25);
-const AdaptedScriptEntity = ModAPI.requireGlobal("Entity");
 const setLoadingTip = ModAPI.requireGlobal("MCSystem.setLoadingTip");
 const Dimension = Native.Dimension;
 const COMBS = [];
-
-Entity.getArmorSlot = function (ent) {
-    return AdaptedScriptEntity.getArmor(ent);
-};
 
 function log(msg, tag) {
     Logger.Log("[" + LOG_TAG + "] " + msg, tag);
