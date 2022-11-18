31c6dd742311d2363ea0d54134b631ebe96107e0
DDShadowRU <ddcompanychanel@gmail.com>
2022-11-18 14:08:17 +0300

    Added copyright header

--- forestry-pe/dev/header.ts
+++ forestry-pe/dev/header.ts
@@ -1,3 +1,26 @@
+/*
+ *  ___               _            ___ ___
+ * | __|__ _ _ ___ __| |_ _ _ _  _| _ \ __|
+ * | _/ _ \ '_/ -_|_-<  _| '_| || |  _/ _|
+ * |_|\___/_| \___/__/\__|_|  \_, |_| |___|
+ *                            |__/
+ *
+ * Github Repository: https://github.com/DDCompany/ForestryPE
+ * Issues Tracker: https://github.com/DDCompany/ForestryPE/issues
+ *
+ * Terms of use:
+ *  - Forbidden to distribute the library on third-party sources
+      without links to the official group (https://vk.com/forestry_pe)
+ *  - Forbidden to change the code of this mod
+ *  - Forbidden to explicitly copy the code to other libraries or mods
+ *  - Using the mod you automatically agree to the conditions described above
+ *
+ * Warning!
+ * All ideas and textures belong to the original author - https://github.com/ForestryMC/ForestryMC
+ *
+ * Port by DDCompany (https://vk.com/forestry_pe)
+ */
+
 IMPORT("EnergyNet");
 IMPORT("flags");
 IMPORT("ToolLib");
